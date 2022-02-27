681c74c90f1c851ffaa1123c2f3e7c8cd9a715a6
Stephan Klinger <staeff@arcor.de>
2022-02-27 12:05:04 +0100

    Format views with black

--- main/views.py
+++ main/views.py
@@ -9,25 +9,31 @@ from .tasks import get_data
 
 
 def convertJSON():
-    with open('./data/lor_plr.geojson') as json_file:
+    with open("./data/lor_plr.geojson") as json_file:
         geojson = json.load(json_file)
         transformer = Transformer.from_crs("EPSG:25833", "EPSG:4326")
         for feature in geojson["features"]:
             convertedCoordinates = list(
-                map(lambda x: transformer.transform(x[0], x[1]), feature["geometry"]["coordinates"][0]))
+                map(
+                    lambda x: transformer.transform(x[0], x[1]),
+                    feature["geometry"]["coordinates"][0],
+                )
+            )
             feature["geometry"]["coordinates"][0] = list(
-                map(lambda x: [round(x[1], 4), round(x[0], 4)], convertedCoordinates))
+                map(lambda x: [round(x[1], 4), round(x[0], 4)], convertedCoordinates)
+            )
 
-        with open('./data/lor_plr_min.geojson', 'w') as outfile:
+        with open("./data/lor_plr_min.geojson", "w") as outfile:
             json.dump(geojson, outfile)
 
 
 def main_view(request):
     context = {}
-    #get_data(schedule=10, repeat=86400)
-    #convertJSON()
+    # get_data(schedule=10, repeat=86400)
+    # convertJSON()
     context["data_unavailable"] = models.Report.objects.filter(
-        createdDay__gte=datetime.date.today() - datetime.timedelta(days=1)).count()
+        createdDay__gte=datetime.date.today() - datetime.timedelta(days=1)
+    ).count()
     return render(request, "main.html", context)
 
 
@@ -64,4 +70,6 @@ class datenListView(ListView):
     ordering = ["createdDay"]
     paginate_by = 20
     template_name = "daten.html"
-    queryset = models.Report.objects.filter(createdDay__gte=datetime.date.today() - datetime.timedelta(days=1))
+    queryset = models.Report.objects.filter(
+        createdDay__gte=datetime.date.today() - datetime.timedelta(days=1)
+    )
